66b7d773fac6ba768a09f88bacb6962776c36588
hujiyo <hj18914255909@outlook.com>
2025-01-24 16:09:41 +0800

    2501241609

---
+++ Mhuixs/mlib/session/session-share.c
@@ -0,0 +1,15 @@
+#include "session.h"
+
+/*
+#版权所有 (c) Mhuixs-team 2024
+#许可证协议:
+#任何人或组织在未经版权所有者同意的情况下禁止使用、修改、分发此作品
+start from 2025.1
+Email:hj18914255909@outlook.com
+*/
+
+/*
+session-share.c
+在linux和windows平台上都可以使用的库
+*/
+
